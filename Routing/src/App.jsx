26fabca957ca9abf01b14864016661df84ad2fce
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-31 21:11:19 +0530

    Link

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
 function App() {
   
 
   return (
     <div>
+
+    
+
       <BrowserRouter>
+
+      <div>
+      <Link to="/blogs1">Blogs 1</Link>
+      <br />
+      <Link to="/blogs2">Blogs 2</Link>
+    </div>
+
         <Routes>
           <Route path="/blogs1" element={<Message1 />} />
           <Route path="/blogs2" element={<Message2 />} />
