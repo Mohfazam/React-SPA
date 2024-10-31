5ace0a27abd791c907b3e0e32d2b4850264d1b85
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-31 20:19:00 +0530

    Basic Routes Setup

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,12 +1,34 @@
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   
 
   return (
     <div>
-      Yo Nigga
+      <BrowserRouter>
+        <Routes>
+          <Route path="/blogs1" element={<Message1 />} />
+          <Route path="/blogs2" element={<Message2 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+
+  function Message1() {
+    return (
+      <div>
+        Hello World 1
+      </div>
+    );
+  }
+
+  function Message2() {
+    return (
+      <div>
+        Hello World 2
+      </div>
+    );
+  }
 
 }
 
