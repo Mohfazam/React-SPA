02ade8f5708bbf437e9911cc000ba5530f1d6d74
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-31 22:03:40 +0530

    Layout

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet} from "react-router-dom";
 function App() {
   
 
@@ -10,21 +10,61 @@ function App() {
 
       <BrowserRouter>
 
-      <div>
-      <Link to="/blogs1">Blogs 1</Link>
-      <br />
-      <Link to="/blogs2">Blogs 2</Link>
-    </div>
+      
 
         <Routes>
-          <Route path="/blogs1" element={<Message1 />} />
-          <Route path="/blogs2" element={<Message2 />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Error />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/blogs1" element={<Message1 />} />
+            <Route path="/blogs2" element={<Message2 />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Route>
         </Routes>
+
       </BrowserRouter>
     </div>
   );
+
+
+  function Header(){
+    return (
+      <div>
+        <Link to="/blogs1">Blogs 1</Link>
+        <br />
+        <Link to="/blogs2">Blogs 2</Link>
+      </div>
+    );
+  }
+
+
+  function Footer(){
+    return (
+      <div>
+        
+        Footer | Contact us | About us
+      </div>
+    );
+  }
+
+  function Layout(){
+    return (
+      <div style={{height: "100vh", backgroundColor: "aqua"}}>
+
+        <div backgroundColor="blue" style={{height: "10vh", backgroundColor: "blue", marginTop: 10}}>
+        <Header />
+        </div>
+
+        <div style={{height: "90vh", backgroundColor: "red"}}>
+          <Outlet />
+        </div>
+      
+        <div style={{backgroundColor: "blue", height: "10vh"}}>
+        <Footer />
+        </div>
+
+      </div>
+    );
+  }
 
   function Error(){
     return (
@@ -61,7 +101,7 @@ function App() {
     return (
       <div>
         Hello World 2
-        <button onClick={redirectUser}>Click here niggaaa</button>
+        <button onClick={redirectUser}>Click here</button>
       </div>
     );
   }
