527e2a78d63b6f15aba50788541eaeab89c057a6
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-01 13:41:20 +0530

    useRef Hook

---
+++ useRef/src/App.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+
+
+function App() {
+  const inputref = useRef();
+  function focusOnInput() {
+    inputref.current.focus();
+  }
+
+  return (
+    <div>
+      Sign Up
+      <input ref={inputref} type="text" id="name" />
+      <input type="text" />
+      <button onClick={focusOnInput}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
