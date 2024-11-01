be1fc47c72e9332a9389af8a5b167e6682625f5e
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-01 21:14:55 +0530

    Bulb Toggle Functionality

--- State1/src/App.jsx
+++ State1/src/App.jsx
@@ -3,7 +3,41 @@ import { useState } from "react";
 function App(){
   return (
     <div>
+      <LightBulb />
+    </div>
+  );
+}
 
+function LightBulb(){
+  const [bulbOn, setBulbOn] = useState(false);
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState setBulbOn={setBulbOn} bulbOn={bulbOn} />
+    </div>
+  );
+}
+
+
+
+function BulbState({bulbOn}){
+
+  return(
+    <div>
+      {bulbOn ? "Bulb is On" : "Bulb is off"}
+    </div>
+  );
+}
+
+function ToggleBulbState({setBulbOn}){
+
+  function toggle(){
+    setBulbOn(currentState => !currentState);
+  }
+
+  return(
+    <div>
+      <button onClick={toggle}>Toggle The Bulb</button>
     </div>
   );
 }
