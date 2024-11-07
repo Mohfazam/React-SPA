90092bf5309c7e0579f278940a2b7ccf2dc93acb
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-07 21:28:58 +0530

    Backend data fetched using custom hook

--- useFetch/src/App.jsx
+++ useFetch/src/App.jsx
@@ -4,10 +4,16 @@ import "./App.css"
 import { useFetch } from './hooks/useFetch'
 
 function App() {
-  const { finalData } = useFetch("https://jsonplaceholder.typicode.com/posts/1");
+  const [currentPost, setCurrentPost] = useState(1);
+  const { finalData } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
 
   return (
     <div>
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>3</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      <br />
+      <br />
       {JSON.stringify(finalData)};
     </div>
   )
