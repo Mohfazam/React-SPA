6c1c66968b11c2d4030f0bcdf9393a414e908c6c
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-08 01:11:48 +0530

    useRef

--- useFetch/src/App.jsx
+++ useFetch/src/App.jsx
@@ -2,27 +2,45 @@
 import { useState, useEffect } from 'react'
 import "./App.css"
 import { useFetch } from './hooks/useFetch'
+import { usePrev } from './hooks/usePrev';
 
 function App() {
-  const [currentPost, setCurrentPost] = useState(1);
-  const { finalData, Loading } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+  // const [currentPost, setCurrentPost] = useState(1);
+  // const { finalData, Loading } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
 
-  if(Loading) {
-    return (
-      <div>Yo its loading... </div>
-    )
-  }
+  // if(Loading) {
+  //   return (
+  //     <div>Yo its loading... </div>
+  //   )
+  // }
 
-  return (
+  // return (
+  //   <div>
+  //     <button onClick={() => setCurrentPost(1)}>1</button>
+  //     <button onClick={() => setCurrentPost(2)}>3</button>
+  //     <button onClick={() => setCurrentPost(3)}>3</button>
+  //     <br />
+  //     <br />
+  //     {JSON.stringify(finalData)};
+  //   </div>
+  // )
+
+
+  const [State, setState] = useState(60);
+  const prev = usePrev(State);
+
+  return(
     <div>
-      <button onClick={() => setCurrentPost(1)}>1</button>
-      <button onClick={() => setCurrentPost(2)}>3</button>
-      <button onClick={() => setCurrentPost(3)}>3</button>
+      <div>
+      {State}
+      </div>
       <br />
-      <br />
-      {JSON.stringify(finalData)};
+      <button onClick = {() => {
+        setState(State + 1)
+      }}>Click Me</button>
+      <p>The Previous State is: {prev}</p>
     </div>
-  )
+  );
 }
 
 export default App;
