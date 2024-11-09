6152f74305d55446ab7f50fd7e337cbef812337b
Mohfazam <mohd.sarwar.code@gmail.com>
2024-11-09 19:44:22 +0530

    Debounce Function

--- useDebounce/src/App.jsx
+++ useDebounce/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function useDebounce(originalFn){
+  const currentClock = useRef();
+
+  const fn = () => {
+    clearTimeout(currentClock.current);
+    currentClock.current = setTimeout(originalFn, 200);
+  }
+
+  return fn;
 }
 
-export default App
+function App() {
+
+  function sendDataToBackend(){
+    fetch('https://jsonplaceholder.typicode.com/posts/1');
+  }
+
+  const debounceFn = useDebounce(sendDataToBackend);
+
+  return (
+    <div>
+      <input type="text" onChange={debounceFn} />
+    </div>
+  );
+}
+
+export default App;
